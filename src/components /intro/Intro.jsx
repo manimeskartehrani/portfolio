e796a39aca723d89638e410f169820b4d19e0604
Mani <apple@Manis.local>
2022-03-17 13:48:22 -0700

    Update picture CSS

--- src/components /intro/Intro.jsx
+++ src/components /intro/Intro.jsx
@@ -20,7 +20,7 @@ export default function Intro() {
         <div className='intro' id="intro">
             <div className="left">
                 <div className="imgContainer">
-                    <img src="./assets/mani.jpeg" alt="" />
+                    <img src="./assets/Mani.jpeg" alt="" />
                 </div>
             </div>
             <div className="right">
