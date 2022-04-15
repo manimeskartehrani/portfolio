971a96a15e26a4e0c4a15d5e86d525c8fdc78ef8
Mani <apple@Manis.local>
2022-04-14 21:24:57 -0700

    Update CV

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import Topbar from "./components /topbar/Topbar";
-import Intro from './components /intro/Intro';
-import Portfolio from './components /portfolio/Portfolio';
-import Works from './components /works/Works';
-import Testimonials from './components /testimonials/Testimonials';
-import Contact from './components /contact/Contact';
-import './app.scss';
-import { useState } from 'react';
-import Menu from "./components /menu/Menu";
-
+import Topbar from "./components /topbar/Topbar"
+import Intro from "./components /intro/Intro"
+import Portfolio from "./components /portfolio/Portfolio"
+import Works from "./components /works/Works"
+import Testimonials from "./components /testimonials/Testimonials"
+import Contact from "./components /contact/Contact"
+import "./app.scss"
+import { useState } from "react"
+import Menu from "./components /menu/Menu"
 
 function App() {
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
-    <div className="app">
-
+    <div className='app'>
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <div className="sections">
+      <div className='sections'>
         <Intro />
         <Portfolio />
         <Works />
@@ -24,7 +22,7 @@ function App() {
         <Contact />
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
